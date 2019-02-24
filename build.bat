07e66fde53a545a66eb7045bec64f653d694b484
Andreas Rohleder <andreas@rohleder.cc>
2019-02-24 16:07:10 +0100

    Release 1.0.3 + added SourceLink.

--- build.bat
+++ build.bat
@@ -1,7 +1,5 @@
 @echo off
 chcp 1252
-call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Preview\Common7\Tools\VsDevCmd.bat" 2> nul
-
 if "%VisualStudioVersion%"=="" call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat" 2> nul
 if "%VisualStudioVersion%"=="" call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat" 2> nul
 if "%VisualStudioVersion%"=="" call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\Common7\Tools\VsDevCmd.bat" 2> nul
@@ -16,3 +14,5 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 rem msbuild /p:Configuration=Release /p:Platform="Any CPU" documentation.shfbproj
 rem if %errorlevel% neq 0 exit /b %errorlevel%
+
+Test\bin\Release\net45\Test.exe
