52a07bc08b43dfcfd3823b16e233de6d7fb46f1e
Gernot Lenkner <g.lenkner@outlook.de>
2020-07-29 17:14:22 +0200

    wip

---
+++ Test/MySqlTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Cave;
+using Cave.Data;
+using Cave.Data.Mysql;
+
+namespace Test.Cave.Data
+{
+#if NET45
+    [TestFixture]
+    class MySqlTests
+    {
+        IStorage mySqlStore = null;
+
+        [OneTimeSetUp]
+        public void ClassInit()
+        {
+            using (var conn = new MySql.Data.MySqlClient.MySqlConnection()) {}
+            mySqlStore = new MySqlStorage("connection");
+        }
+
+        [Test]
+        public void CheckLayout()
+        {
+            if (mySqlStore != null)
+            {
+
+            }
+        }
+
+    }
+#endif
+}
